0d0d92ad89a1c4dab0244a404ce856d20ae4d138
Agencia-pv <contato.agenciavp@gmail.com>
2021-01-10 11:41:29 -0300

    Olha aqui pedro

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,34 @@ import { ServerStyleSheets } from "@material-ui/styles";
 class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
+      <html lang="pt-br">
         <Head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LN8RM5N1N3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LN8RM5N1N3');
+</script>
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '402770304289550');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=402770304289550&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
           <meta charSet="utf-8" />
           <meta
             name="viewport"
