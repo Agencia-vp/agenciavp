775f9cf10a8cbdfd1e78d3c1c3807b89ac4cf305
Agencia-pv <contato.agenciavp@gmail.com>
2021-01-10 16:50:45 -0300

    oi

--- assets/jss/nextjs-material-kit/pages/landingPage.js
+++ assets/jss/nextjs-material-kit/pages/landingPage.js
@@ -1,7 +1,5 @@
 import { container, title } from "assets/jss/nextjs-material-kit.js";
 
-
-import imagesStyle from "assets/jss/nextjs-material-kit/imagesStyles.js";
 const landingPageStyle = {
   container: {
     zIndex: "12",
@@ -27,30 +25,6 @@ const landingPageStyle = {
     position: "relative",
     zIndex: "3"
   },
-    description: {
-    margin: "1.071rem auto 0",
-    maxWidth: "600px",
-    color: "#999",
-    textAlign: "center !important"
-  },
-  name: {
-    marginTop: "-80px"
-  },
-  socials: {
-    marginTop: "0",
-    width: "100%",
-    transform: "none",
-    left: "0",
-    top: "0",
-    height: "100%",
-    lineHeight: "41px",
-    fontSize: "20px",
-    color: "#999"
-  },
-  navWrapper: {
-    margin: "20px auto 50px auto",
-    textAlign: "center"
-  },
   mainRaised: {
     margin: "-60px 30px 0px",
     borderRadius: "6px",
@@ -58,7 +32,5 @@ const landingPageStyle = {
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
   }
 };
-
-
 
 export default landingPageStyle;
